eb5ef050c1ca9faa57a81faf739e266c5cfcf471
GuanJunMa <guanjun_ma@LGNB2022090012.local>
2024-08-21 00:06:49 +0800

    增加外链

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="pt-8 pb-16 bg-gray-50 border-t border-gray-200">
       <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
         <div className="grid grid-cols-2 gap-12 sm:grid-cols-3 lg:grid-cols-4 lg:mt-10">
-          <div className="col-span-2 sm:col-span-3 lg:col-span-2">
+          <div className="col-span-2 sm:col-span-3 lg:col-span-1 relative custom-top-negative">
             <a href="/" className="flex nuxt-link-active">
               <img src='./logo_horizontal.png' alt="" className="h-20 w-auto" />
             </a>
@@ -14,7 +14,7 @@ export default function Footer() {
               Copyright 2024 © HANSMARK LTD. All rights reserved.
             </p>
           </div>
-          <div className="lg:pl-24">
+          <div className="lg:pl-2">
             <p className="text-xs font-medium tracking-widest text-primary-500 uppercase">Contact Us</p>
             <ul className="mt-8 space-y-5">
               <li>
@@ -24,13 +24,85 @@ export default function Footer() {
               </li>
             </ul>
           </div>
-          <div className="lg:pl-24">
+          <div className="lg:pl-1">
             <p className="text-xs font-medium tracking-widest text-primary-500 uppercase">Links</p>
-            <ul className="mt-8 space-y-5">
+            <ul className="mt-8 space-y-1">
               <li>
-                <a href="/" className="inline-flex text-sm font-normal text-gray-600  hover:text-gray-900">
-                  Blog
-                </a>
+                <a className="inline-flex text-sm font-normal text-gray-600  hover:text-gray-900" target="_blank" href="https://funai.online/" title="Fun AI Tools Directory">Fun AI Tools Diresctory</a>
+              </li>
+              <li>
+                <a className="inline-flex text-sm font-normal text-gray-600  hover:text-gray-900" target="_blank" href="https://right-ai.com/" title="RightAI Tools Directory">RightAI Tools Diresctory</a>
+              </li>
+              <li>
+                <a className="inline-flex text-sm font-normal text-gray-600  hover:text-gray-900" target="_blank" href="https://aijustworks.com" title="AI Just Works">AI Just Works</a>
+              </li>
+              <li>
+                <a className="inline-flex text-sm font-normal text-gray-600  hover:text-gray-900" target="_blank" href="https://toolsapp.cc/" title="ToolsApp AI Tools Directory">ToolsApp AI Tools Diresctory</a>
+              </li>
+              <li>
+                <a className="inline-flex text-sm font-normal text-gray-600  hover:text-gray-900" target="_blank" title="The Best AI Tools" href="https://allinai.tools">All in AI Tools</a>
+              </li>
+              <li>
+                <a className="inline-flex text-sm font-normal text-gray-600  hover:text-gray-900" target="_blank" href="https://magicbox.tools/" title="MagicBox.Tools - AI Tools Directory">MagicBox.Tools - AI Tools Diresctory</a>
+              </li>
+              <li>
+                <a className="inline-flex text-sm font-normal text-gray-600  hover:text-gray-900" target="_blank" href="https://www.dir2ai.com" title="Dir2AI Tools Directory">Dir2AI</a>
+              </li>
+              <li>
+                <a className="inline-flex text-sm font-normal text-gray-600  hover:text-gray-900" target="_blank" href="https://aiprds.top/" title="AIPrds AI Directory">AI Prds</a>
+              </li>
+              <li>
+                <a className="inline-flex text-sm font-normal text-gray-600  hover:text-gray-900" target="_blank" href='https://aitooltrek.com' title='AI Tool Trek'> AI Tool Trek </a>
+              </li>
+              <li>
+                <a className="inline-flex text-sm font-normal text-gray-600  hover:text-gray-900" target="_blank" href="https://woy.ai/" title="Woy AI Tools Directory">Woy AI Tools</a>
+              </li>
+              <li>
+                <a className="inline-flex text-sm font-normal text-gray-600  hover:text-gray-900" target="_blank" href="https://dokeyai.com/" title="Dokey AI Tools Directory">DokeyAI</a>
+              </li>
+              <li>
+                <a className="inline-flex text-sm font-normal text-gray-600  hover:text-gray-900" target="_blank" href="https://iuu.ai/" title="iuu AI">iuu AI</a>
+              </li>
+            </ul>
+          </div>
+          <div className="lg:pl-1">
+            <p className="text-xs font-medium tracking-widest text-primary-500 uppercase">&nbsp;</p>
+            <ul className="mt-8 space-y-1">
+              <li>
+                <a className="inline-flex text-sm font-normal text-gray-600  hover:text-gray-900" href="https://dang.ai/" target="_blank" title="https://dang.ai/">dang.ai</a>
+              </li>
+              <li>
+                <a className="inline-flex text-sm font-normal text-gray-600  hover:text-gray-900" target="_blank" href="https://aistage.net" title="AIStage">AIStage</a>
+              </li>
+              <li>
+                <a className="inline-flex text-sm font-normal text-gray-600  hover:text-gray-900" target="_blank" href="https://t0ai.com" title="T0 AI Tools Directory">T0 AI Tools Directory</a>
+              </li>
+              <li>
+                <a className="inline-flex text-sm font-normal text-gray-600  hover:text-gray-900" target="_blank" href="https://navs.site" title="2024年最佳和最新的AI导航网站">Navs Site</a>
+              </li>
+              <li>
+                <a className="inline-flex text-sm font-normal text-gray-600  hover:text-gray-900" target="_blank" href="https://aitoolsexplorer.org/" title="AI Tools Explorer">AI Tools Explorer</a>
+              </li>
+              <li>
+                <a className="inline-flex text-sm font-normal text-gray-600  hover:text-gray-900" target="_blank" href="https://bai.tools" target="_blank">BAI.tools</a>
+              </li>
+              <li>
+                <a className="inline-flex text-sm font-normal text-gray-600  hover:text-gray-900" target="_blank" href="https://aitoolcenter.com/" title="AI Tool Center">AI Tool Center</a>
+              </li>
+              <li>
+                <a className="inline-flex text-sm font-normal text-gray-600  hover:text-gray-900" target="_blank" href="https://whatisaitools.com/" title="What Is Ai Tools">What Is Ai Tools</a>
+              </li>
+              <li>
+                <a className="inline-flex text-sm font-normal text-gray-600  hover:text-gray-900" target="_blank" href="https://www.aigotools.com" title="AigoTools">AigoTools</a>
+              </li>
+              <li>
+                <a className="inline-flex text-sm font-normal text-gray-600  hover:text-gray-900" target="_blank" href="https://soai.tools" title="SoaiTools">SoaiTools</a>
+              </li>
+              <li>
+                <a className="inline-flex text-sm font-normal text-gray-600  hover:text-gray-900" target="_blank" href="https://tap4.ai/" title="Tap4 AI Tools Directory">Tap4 AI Tools Diresctory</a>
+              </li>
+              <li>
+                <a className="inline-flex text-sm font-normal text-gray-600  hover:text-gray-900" target="_blank" href="https://aistage.net" title="AIStage">AIStage</a>
               </li>
             </ul>
           </div>
